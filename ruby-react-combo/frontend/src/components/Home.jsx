c62c70ec090cdf46f007c7cee66211816f45779a
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-13 19:02:17 -0400

    add bootstrap css

--- ruby-react-combo/frontend/src/components/Home.jsx
+++ ruby-react-combo/frontend/src/components/Home.jsx
@@ -2,28 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Season from "./Season";
 import TopDrinks from "./TopDrinks";
+import "./Home.scss"
 
 export default () => (
   <div>
 
-    <div className="homepage-image-container">
+    {/* <div className="homepage-image-container">
       <img src={'/images/homepage.jpg'} alt="cocktail homepage" />
-    </div>
+    </div> */}
 
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Cocktail Shaker</h1>
-        <p className="lead">
-          Recipes and more
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/Drinks"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          Start Making Drinks
-        </Link>
+    <div className="homepage-image-container">
+      <div className="homepage-image-text">
+        <div className="container secondary-color">
+          <h1 className="display-4">Cocktail Shaker</h1>
+          <p className="lead">
+            Recipes and more
+          </p>
+          <hr className="my-4" />
+          <Link
+            to="/DrinkMixer"
+            className="btn btn-lg custom-button"
+            role="button"
+          >
+            Start Making Drinks
+          </Link>
+        </div>
       </div>
     </div>
 
