84543e607e0d2ff1f4b3251e55cc4abadd20ce63
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-14 16:16:09 -0400

    edit css

--- ruby-react-combo/frontend/src/components/Drinks.jsx
+++ ruby-react-combo/frontend/src/components/Drinks.jsx
@@ -13,19 +13,49 @@ export default function Drinks() {
       <Link to={`/drinks/${drink.id}`}>
         <img src={drink.image} width="200" alt={drink.name} />
         <p>{drink.name} - {drink.tags}</p>
-        
       </Link>
     </li>
   ));
 
   return (
     <div>
-      <div>Drink list</div>
-      <div>
+      <h1>Drink list</h1>
         <ul>
           {drinkList}
         </ul>
-      </div>
     </div>
   );
 }
+
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios"
+// import useApplicationData from "../hooks/useApplicationData";
+
+// export default () => {
+// 	//   const [drinks, setDrinks] = useState([]);
+// 	//   useEffect(() => {
+// 	//     axios.get("/drinks.json").then((res) => {
+// 	//       setDrinks(res.data);
+// 	//     });
+// 	//   }, []);
+
+// 	const { state } = useApplicationData();
+// 	const drinks = [...state.drinks];
+
+// 	const drinkList = drinks.map(drink => {
+// 		return (
+// 			<div key={drink.id} >
+// 				<h1>{drink.name}</h1>
+// 				<img className="drink-list-img" src={drink.image}></img>
+// 			</div>
+// 		)
+// 	});
+
+// 	return (
+// 		<div>
+// 			<h1>drinks</h1>
+// 			<ul>{drinkList}</ul>
+// 		</div>
+// 	);
+// };
