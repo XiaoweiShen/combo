eb09680224a6a9860c1584096ca2e7fdc954f6ec
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-15 05:18:50 -0400

    edit

--- ruby-react-combo/frontend/src/routes/Index.jsx
+++ ruby-react-combo/frontend/src/routes/Index.jsx
@@ -6,14 +6,16 @@ import Navigation from "../components/Navigation";
 import DrinkMixer from "../components/DrinkMixer";
 import Drink from "../components/Drink";
 
-export default (
-	<Router>
-		<Navigation />
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/Drinks" element={<Drinks />} />
-			<Route path="/Drinks/:id" element={<Drink />} />
-			<Route path="/DrinkMixer" element={<DrinkMixer />} />
-		</Routes>
-	</Router>
-);
+export default function App() {
+	return (
+		<Router>
+			<Navigation />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/Drinks" element={<Drinks />} />
+				<Route path="/Drinks/:id" element={<Drink />} />
+				<Route path="/DrinkMixer" element={<DrinkMixer />} />
+			</Routes>
+		</Router>
+	)
+}
