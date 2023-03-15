414728e75561c2b8382796fe8e116af20f6eea0d
Janet <Janet_Lam@live.ca>
2023-03-15 06:46:10 -0400

    Merged Cat and my branch
    Co-authored-by: Catherine Wu <Catmhhw@users.noreply.github.com>

--- ruby-react-combo/frontend/src/components/Drinks.jsx
+++ ruby-react-combo/frontend/src/components/Drinks.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import React from "react";
-import {Route, Link, Routes, useParams} from 'react-router-dom';
+import { Route, Link, Routes, useParams } from 'react-router-dom';
 import useApplicationData from "../hooks/useApplicationData";
-
+import "./Drinks.scss"
 
 export default function Drinks() {
-  const {state} = useApplicationData();
+  const { state } = useApplicationData();
   const drinks = [...state.drinks];
 
   const drinkList = drinks.map(drink => (
@@ -13,15 +13,15 @@ export default function Drinks() {
       <Link to={`/drinks/${drink.id}`}>
         <img src={drink.image} width="200" alt={drink.name} />
         <p>{drink.name} - {drink.tags}</p>
-        
+
       </Link>
     </li>
   ));
 
   return (
-    <div>
-      <div>Drink list</div>
-      <div>
+    <div class="drinks-list-body">
+      <h1 class="drinks-list-header">Drink list</h1>
+      <div class="drinks-list-ul">
         <ul>
           {drinkList}
         </ul>
