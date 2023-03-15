fb565b113c7e6feec28d1dc136aac600a1338962
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-15 05:25:52 -0400

    check

--- ruby-react-combo/backend/config/routes.rb
+++ ruby-react-combo/backend/config/routes.rb
@@ -1,16 +1,32 @@
+# Rails.application.routes.draw do
+ 
+#     resources :drinks,only: [:index, :show, :edit, :update,:check_ingredients]
+#     resources :ingredients,only: [:index, :show]
+#     resources :glasses, only: [:index, :show]
+#     resources :drink_ingredients, only: [:index,:show]
+#     get '/api/check/:id', to: 'drinks#check', as:'id'
+#     root 'homepage#index'
+#     get '/api/drinks/:id', to: 'drinks#show'
+#     get '/api/drinks', to: 'drinks#index'
+ 
+#     get '/api/drink_ingredients/:id', to: 'drink_ingredients#show'
+
+#     match '*path' => 'homepage#index', via: :all
+#   end
+  
+
 Rails.application.routes.draw do
  
-    resources :drinks,only: [:index, :show, :edit, :update,:check_ingredients]
-    resources :ingredients,only: [:index, :show]
-    resources :glasses, only: [:index, :show]
-    resources :drink_ingredients, only: [:index,:show]
-    get '/api/check/:id', to: 'drinks#check', as:'id'
-    root 'homepage#index'
-    get '/api/drinks/:id', to: 'drinks#show'
-    get '/api/drinks', to: 'drinks#index'
- 
-    get '/api/drink_ingredients/:id', to: 'drink_ingredients#show'
+  resources :drinks,only: [:index, :show, :edit, :update]
+  resources :ingredients,only: [:index, :show]
+  resources :glasses, only: [:index, :show]
+  resources :drink_ingredients, only: [:index,:show]
+  get '/api/check/:id', to: 'drinks#check', as:'id'
+  get '/api/search/:query', to: 'drinks#search', as:'query'
+  get '/api/search', to: 'drinks#search'
+  root 'homepage#index'
+  get '/api/drinks/:id', to: 'drinks#show'
+  get '/api/drinks', to: 'drinks#index'
 
-    match '*path' => 'homepage#index', via: :all
-  end
-  
+  match '*path' => 'homepage#index', via: :all
+end
