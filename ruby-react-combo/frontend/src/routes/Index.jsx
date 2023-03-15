f2ac7409d081b91b4e969e15fc824ab88e2d3361
Catmhhw <catherine.mhh.wu@gmail.com>
2023-03-15 06:39:42 -0400

    fix

--- ruby-react-combo/frontend/src/routes/Index.jsx
+++ ruby-react-combo/frontend/src/routes/Index.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
+import Drink from "../components/Drink"
 import Drinks from "../components/Drinks";
 import Navigation from "../components/Navigation";
 import DrinkMixer from "../components/DrinkMixer";
-import Drink from "../components/Drink";
+//import DrinkList from "../components/DrinkList";
 
-export default function App() {
-	return (
-		<Router>
-			<Navigation />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/Drinks" element={<Drinks />} />
-				<Route path="/Drinks/:id" element={<Drink />} />
-				<Route path="/DrinkMixer" element={<DrinkMixer />} />
-			</Routes>
-		</Router>
-	)
-}
+export default (
+	<Router>
+		<Navigation />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/Drinks" element={<Drinks />} />
+			<Route path="/Drinks/:id" element={<Drink />} />
+			<Route path="/DrinkMixer" element={<DrinkMixer />} />
+		 </Routes>
+	</Router>
+);
+
+// redirect request to old page
