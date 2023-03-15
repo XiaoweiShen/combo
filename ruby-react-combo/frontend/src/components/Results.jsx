1189f01f7262b7648ada81d5de1bfc6c6d03210c
XiaoweiShen <snhiui@gmail.com>
2023-03-14 22:04:45 -0700

    search bar

--- ruby-react-combo/frontend/src/components/Results.jsx
+++ ruby-react-combo/frontend/src/components/Results.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Card, Col, Row } from 'antd';
+import { Link } from "react-router-dom";
 
 const { Meta } = Card;
 
@@ -9,7 +10,11 @@ export default function Results(props){
     return results.map((drink) => {
     return (
      <Col key={drink.id} xs={24} sm={12} md={6}>
-     <Card  hoverable cover={<img className='drink-card-img' src={drink.image } alt="foo" />}>
+     <Card  hoverable cover={
+     <Link to={`/drinks/${drink.id}`}>
+     <img className='drink-card-img' src={drink.image } alt="foo" />
+     </Link>
+     }>
      <Meta title={drink.name+"---"+drink.tags}/>
      </Card>
      </Col>
